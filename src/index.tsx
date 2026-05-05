c28fd3d70b37b4e254c8a827bb48b213a92602aa
Jomy Jose <jomyjose3009@gmail.com>
2026-05-05 14:46:03 -0400

    theme added

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./app/App";
 import reportWebVitals from "./reportWebVitals";
-import "./assets/variables.css";
+import "./assets/variables.scss";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
--- src/pages/AccountDetails/AccountDetails.tsx
+++ src/pages/AccountDetails/AccountDetails.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { getAccountById } from "./../../api/services";
 import styles from "./AccountDetails.module.scss";
-import { IoArrowBack } from "react-icons/io5";
+// import { IoArrowBack } from "react-icons/io5";
 
 export const AccountDetails = () => {
   const navigate = useNavigate();
@@ -35,7 +35,7 @@ export const AccountDetails = () => {
             className={styles["back-button"]}
             onClick={() => navigate(-1)}
           >
-            <span className={styles["back-icon"]}>{IoArrowBack({})}</span>
+            {/* <span className={styles["back-icon"]}>{IoArrowBack({})}</span> */}
           </button>
           <h1 className={styles["account-details-title"]}>Account Details</h1>
         </div>
