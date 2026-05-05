6b48584b926129a244100cdad34fde3c563d4786
Jomy Jose <jomyjose3009@gmail.com>
2026-05-05 14:03:19 -0400

    theme added

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 import AppFloorplan from "./AppFloorplan";
 import config from "./App.config";
 
 function App() {
+  useEffect(() => {
+    document.documentElement.setAttribute("data-theme", "light");
+  }, []);
+
   return <AppFloorplan floorplanConfig={config.floorplanConfig} />;
 }
 
