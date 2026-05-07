a8371be2d22ac68368528fd53867c2c2ffa0cca8
Jom George <jomgrg@gmail.com>
2026-05-07 13:24:37 -0400

    admin api changes

--- src/api/services/index.ts
+++ src/api/services/index.ts
@@ -2,3 +2,5 @@ export * from "./account/accountApi";
 export * from "./account/types";
 export * from "./job/jobApi";
 export * from "./job/types";
+export * from "./admin/adminApi";
+export * from "./admin/types";
